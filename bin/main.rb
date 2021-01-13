8564dda81ab8c3abc183cbfc735716cb73610478
Oever González <notengobattery@gmail.com>
2021-01-13 16:39:22 -0600

    add basic greeting message in the view

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+
+mock_game_end = false
+mock_player1_symbol = 'X'
+mock_player2_symbol = 'O'
+
+puts '~~~~~~~ Welcome to TicTacToe ~~~~~~~'
+puts
+print 'Enter name for player 1: '
+player1_name = gets.chomp
+puts "Player #{player1_name} is #{mock_player1_symbol}"
+print 'Enter name for player 2: '
+player2_name = gets.chomp
+puts "Player #{player2_name} is #{mock_player2_symbol}"
+
+puts
+puts '~~~~~~~ Get ready to play! ~~~~~~~'
+
+until mock_game_end
+
+end
