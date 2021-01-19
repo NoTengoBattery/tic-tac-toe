53097795edfff4a8b955d7c16c6522c3b55c4043
Oever González <notengobattery@gmail.com>
2021-01-19 15:52:33 -0600

    test: add test cases for the Game class

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -3,6 +3,7 @@
 require_relative '../lib/game_logic'
 
 PLAYER_NAME = 'Frank'.freeze
+PLAYER2_NAME = 'Chris'.freeze
 PLAYER_EMPTY_NAME = " \n\t \t\n ".freeze
 PLAYER_SYMBOL = 'X'.freeze
 PLAYER_ID = 1
@@ -57,22 +58,85 @@ RSpec.describe 'Board' do
   let(:outer) { rand(0..2) }
   let(:inner) { rand(0..2) }
   let(:input) { outer * 3 + inner + 1 }
-  it 'Calculates outer index' do
+  it 'calculates outer index' do
     outer_index, _inner_index = board.calculate_index(input)
     expect(outer_index).to eq(outer)
   end
-  it 'Calculates inner index' do
+  it 'calculates inner index' do
     _outer_index, inner_index = board.calculate_index(input)
     expect(inner_index).to eq(inner)
   end
-  it 'checks if the board is updating the symbol in the correct index' do
+  it 'updates the cell inside the board' do
     outer, inner = board.calculate_index(input)
     board.update_board(outer, inner, PLAYER_SYMBOL)
     expect(board.printable_board[outer][inner]).to eq(PLAYER_SYMBOL)
   end
-  it 'Raise exception if the cell is already taken' do
+  it 'raises an exception if the cell is already taken' do
     outer, inner = board.calculate_index(input)
     board.update_board(outer, inner, PLAYER_SYMBOL)
     expect { board.calculate_index(input) }.to raise_error(StandardError)
+  end
+end
+
+RSpec.describe 'Game' do
+  let(:game) { Game.new }
+  let(:input) { rand(1..9) }
+  it 'register a player and it is not nil' do
+    game.register_player(PLAYER_NAME)
+    expect(game.last_registered_player).not_to be(nil)
+  end
+  it 'register a player and it have the correct name' do
+    game.register_player(PLAYER_NAME)
+    expect(game.last_registered_player.name).to eq(PLAYER_NAME)
+  end
+  it 'register two players' do
+    game.register_player(PLAYER_NAME)
+    player1 = game.last_registered_player
+    game.register_player(PLAYER2_NAME)
+    player2 = game.last_registered_player
+    expect(player1.name).not_to eq(player2.name)
+  end
+  it 'returns a representation of the board' do
+    expect(game.board).is_a?(Array)
+  end
+  it 'increments the turn when the turn is valid' do
+    game.register_player(PLAYER_NAME)
+    game.register_player(PLAYER2_NAME)
+    current_turn = game.turn
+    game.execute_turn(input)
+    next_turn = game.turn
+    expect(next_turn).to eq(current_turn + 1)
+  end
+  it 'repeats the same turn when the turn is invalid' do
+    game.register_player(PLAYER_NAME)
+    game.register_player(PLAYER2_NAME)
+    game.execute_turn(input)
+    current_turn = game.turn
+    begin
+      game.execute_turn(input)
+    rescue StandardError
+      next_turn = game.turn
+    end
+    expect(next_turn).to eq(current_turn)
+  end
+  it 'changes the current player when the turn is valid' do
+    game.register_player(PLAYER_NAME)
+    game.register_player(PLAYER2_NAME)
+    current_player = game.current_player
+    game.execute_turn(input)
+    next_player = game.current_player
+    expect(next_player).not_to eq(current_player)
+  end
+  it 'keeps the same player when the turn is invalid' do
+    game.register_player(PLAYER_NAME)
+    game.register_player(PLAYER2_NAME)
+    game.execute_turn(input)
+    current_player = game.current_player
+    begin
+      game.execute_turn(input)
+    rescue StandardError
+      next_player = game.current_player
+    end
+    expect(next_player).to eq(current_player)
   end
 end
